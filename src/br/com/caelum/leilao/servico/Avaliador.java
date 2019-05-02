98a68ff323a2cb50817b5704a30df46fc4178cb0
Katia Santos Euzebio <katiaseuzebio@gmail.com>
2019-05-02 16:34:50 -0300

    Testing get first three highest bids.

--- src/br/com/caelum/leilao/servico/Avaliador.java
+++ src/br/com/caelum/leilao/servico/Avaliador.java
@@ -1,5 +1,10 @@
 package br.com.caelum.leilao.servico;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 import br.com.caelum.leilao.dominio.Lance;
 import br.com.caelum.leilao.dominio.Leilao;
 
@@ -8,6 +13,7 @@ public class Avaliador {
 	private double maiorDeTodos = Double.NEGATIVE_INFINITY;
 	private double menorDeTodos = Double.POSITIVE_INFINITY;
 	private double mediaLances = 0.0;
+	private List<Lance> maiores;
 
 	public void avalia(Leilao leilao) {
 		double totalLances = 0;
@@ -22,6 +28,21 @@ public class Avaliador {
 			totalLances += lance.getValor();
 		}
 		this.mediaLances = totalLances / leilao.getLances().size();
+		pegaOsMaioresNo(leilao);
+	}
+
+	private void pegaOsMaioresNo(Leilao leilao) {
+		maiores = new ArrayList<Lance>(leilao.getLances());
+		Collections.sort(maiores, new Comparator<Lance>() {
+			public int compare(Lance o1, Lance o2) {
+				if (o1.getValor() < o2.getValor())
+					return 1;
+				if (o1.getValor() > o2.getValor())
+					return -1;
+				return 0;
+			}
+		});
+		maiores = maiores.subList(0, maiores.size() > 3 ? 3 : maiores.size());
 	}
 
 	public double getMaiorLance() {
@@ -33,7 +54,11 @@ public class Avaliador {
 	}
 
 	public double getMediaLance() {
-    	return mediaLances;
-    }
+		return mediaLances;
+	}
+
+	public List<Lance> getMaiores() {
+		return maiores;
+	}
 
 }
--- test/br/com/caelum/leilao/servico/AvaliadorTest.java
+++ test/br/com/caelum/leilao/servico/AvaliadorTest.java
@@ -2,6 +2,8 @@ package br.com.caelum.leilao.servico;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.List;
+
 import org.junit.Test;
 
 import br.com.caelum.leilao.dominio.Lance;
@@ -9,7 +11,7 @@ import br.com.caelum.leilao.dominio.Leilao;
 import br.com.caelum.leilao.dominio.Usuario;
 
 public class AvaliadorTest {
-	
+
 	@Test
 	public void deveEntenderLancesEmOrdemCrescente() {
 		// cenario: 1 lances em ordem crescente
@@ -74,5 +76,69 @@ public class AvaliadorTest {
 		// comparando a saida com o esperado
 		assertEquals(250.0, leiloeiro.getMaiorLance(), 0.00001);
 		assertEquals(250.0, leiloeiro.getMenorLance(), 0.00001);
+	}
+
+	@Test
+	public void testaTresMaiores() {
+		// cenario: 4 Um leilão com 5 lances, deve encontrar os três maiores
+		Usuario joao = new Usuario("Joao");
+		Usuario jose = new Usuario("José");
+		Usuario maria = new Usuario("Maria");
+
+		Leilao leilao = new Leilao("Playstation 3 Novo");
+
+		leilao.propoe(new Lance(maria, 250.0));
+		leilao.propoe(new Lance(joao, 700.0));
+		leilao.propoe(new Lance(jose, 400.0));
+		leilao.propoe(new Lance(jose, 500.0));
+		leilao.propoe(new Lance(jose, 100.0));
+
+		// executando a acao
+		Avaliador leiloeiro = new Avaliador();
+		leiloeiro.avalia(leilao);
+
+		// comparando a saida com o esperado
+		List<Lance> maiores = leiloeiro.getMaiores();
+		assertEquals(3, maiores.size());
+		assertEquals(700, maiores.get(0).getValor(), 0.0001);
+		assertEquals(500, maiores.get(1).getValor(), 0.0001);
+		assertEquals(400, maiores.get(2).getValor(), 0.0001);
+	}
+
+	@Test
+	public void testaTresMaioresListaMenor() {
+		// cenario: 5 Um leilão com 2 lances, deve devolver apenas os dois lances que
+		// encontrou
+		Usuario joao = new Usuario("Joao");
+		Usuario maria = new Usuario("Maria");
+
+		Leilao leilao = new Leilao("Playstation 3 Novo");
+
+		leilao.propoe(new Lance(maria, 250.0));
+		leilao.propoe(new Lance(joao, 300.0));
+
+		// executando a acao
+		Avaliador leiloeiro = new Avaliador();
+		leiloeiro.avalia(leilao);
+
+		// comparando a saida com o esperado
+		List<Lance> maiores = leiloeiro.getMaiores();
+		assertEquals(2, maiores.size());
+		assertEquals(300, maiores.get(0).getValor(), 0.0001);
+		assertEquals(250, maiores.get(1).getValor(), 0.0001);
+	}
+
+	@Test
+	public void testaTresMaioresListaVazia() {
+		// cenario: 6 Um leilão sem nenhum lance, devolve lista vazia
+		Leilao leilao = new Leilao("Playstation 3 Novo");
+
+		// executando a acao
+		Avaliador leiloeiro = new Avaliador();
+		leiloeiro.avalia(leilao);
+
+		// comparando a saida com o esperado
+		List<Lance> maiores = leiloeiro.getMaiores();
+		assertEquals(0, maiores.size());
 	}
 }
