85b371efb3986a7508f986b90d565d4bfcc4e25f
Katia Santos Euzebio <katiaseuzebio@gmail.com>
2019-05-03 18:25:19 -0300

    Developing and testing role double bid

--- src/br/com/caelum/leilao/dominio/Leilao.java
+++ src/br/com/caelum/leilao/dominio/Leilao.java
@@ -3,6 +3,7 @@ package br.com.caelum.leilao.dominio;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Leilao {
 
@@ -39,6 +40,18 @@ public class Leilao {
 
 	public List<Lance> getLances() {
 		return Collections.unmodifiableList(lances);
+	}
+
+	public void dobraLance(Usuario usuario) {
+		double valorUltimoLance = getValorUltimoLance(usuario);
+		Lance novoLance = new Lance(usuario, valorUltimoLance * 2);
+		lances.add(novoLance);
+	}
+
+	private double getValorUltimoLance(Usuario usuario) {
+		List<Lance> lancesUsuario = lances.stream().filter(l -> usuario.equals(l.getUsuario()))
+				.collect(Collectors.toList());
+		return lancesUsuario.get(lancesUsuario.size() - 1).getValor();
 	}
 
 }
--- test/br/com/caelum/leilao/dominio/LeilaoTest.java
+++ test/br/com/caelum/leilao/dominio/LeilaoTest.java
@@ -47,5 +47,29 @@ public class LeilaoTest {
 		assertEquals(10, leilao.getLances().size());
 		assertEquals(8000.00, leilao.getLances().get(9).getValor(), 0.00001);
 	}
+	
+	@Test
+	public void deveDobrarLance() {
+		Leilao leilao = new Leilao("Leilao I");
 
+		Usuario steve = new Usuario("Steve Jobs");
+		Usuario bill = new Usuario("Bill Gates");
+
+		leilao.propoe(new Lance(steve, 3000.00));
+		leilao.propoe(new Lance(bill, 2000.00));
+		
+		leilao.propoe(new Lance(steve, 4000.00));
+		leilao.propoe(new Lance(bill, 2000.00));
+		
+		leilao.propoe(new Lance(steve, 3000.00));
+		leilao.propoe(new Lance(bill, 6000.00));
+		
+		leilao.dobraLance(steve);
+
+		assertEquals(7, leilao.getLances().size());
+		assertEquals(6000.00, leilao.getLances().get(6).getValor(), 0.00001);
+	}
+	
+	
+	
 }
